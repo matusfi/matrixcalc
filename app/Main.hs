2f1cedfec7c93a9f2d87b4e289aaeb0f6941a313
Matus Laslofi <matus.laslofi@gmail.com>
2016-03-23 18:10:32 +0100

    Get url in arg and parse it

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Lib (Matrix, Error)
+import Network.HTTP.Conduit
+import System.Environment (getArgs)
+import qualified Data.ByteString.Lazy as L
+import Control.Monad.IO.Class (liftIO)
 
 main :: IO ()
-main = print "Hello!"
+main = do
+  args <- getArgs
+  case args of
+    [urlString] -> runTests urlString
+    _           -> error "Please enter only one URL"
+
+runTests :: String -> IO ()
+runTests urlString = case parseUrl urlString of
+                       Nothing  -> error "Invalid URL"
+                       Just req -> putStrLn "Yeeehaa!"
