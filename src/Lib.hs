94688fe9478be1234c59fe30e02b3fc8078e5f70
Matus Laslofi <matus.laslofi@gmail.com>
2016-03-23 16:18:18 +0100

    JSON parsing using generics

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,7 @@
 
 module Lib where
 
+import GHC.Generics
 import Data.List
 import Data.Aeson
 import Data.ByteString.Lazy.Char8
@@ -10,15 +11,11 @@ import qualified Data.ByteString.Lazy as B
 
 data Matrix = Matrix {
                 matrix :: [[Int]]
-              } deriving Show
+              } deriving (Show, Generic)
+
+instance FromJSON Matrix
+instance ToJSON Matrix
 
 mtx :: B.ByteString
 mtx = "{\"matrix\": [[1,2,3],[4,5,6],[7,8,9]]}" 
 
-instance FromJSON Matrix where
-  parseJSON (Object v) = Matrix <$>
-                         v .: "matrix"
-
-instance ToJSON Matrix where
-  toJSON (Matrix matrix) = 
-    object [ "matrix" .= matrix ]  
